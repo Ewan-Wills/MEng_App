d223a8496a8c7b6aa93da93a869e6fb1f363c2d8
Ewan-Wills <ewills172@gmail.com>
2025-02-09 10:11:09 +0000

    live updating graph with labelled x-axis

--- tauri-app/src/main.js
+++ tauri-app/src/main.js
@@ -19,36 +19,79 @@ window.addEventListener("DOMContentLoaded", () => {
 
 // Set Dimensions
 
-const xMax = 500;
-const yMax = 400;
+const xSize = 500;
+const ySize = 500;
+const margin = 100;
+const xMax = xSize - margin*2;
+const yMax = ySize - margin*2;
+
 var numPoints = 0;
 const maxPoints = 10;
-var dataPacket = {'index':Number, 'time':String, 'temp':Number}
+var currSize;
+//var dataPacket = {'index':index(),'packetNumber':Number, 'time':String, 'temp':Number};
 var dataPacketList = [];
-async function plot() {  
+
+function updateIndex(){
+  for (var i=0; i<=(maxPoints-1);i++){
+      if (dataPacketList.at(i)){
+        dataPacketList.at(i).index=i;    
+      }
+    }
+  
+}
+async function plot() {    
+
+  var thisDataPacket = {'index': Number,'packetNumber':Number, 'time':String, 'temp':Number};
+  
+  thisDataPacket.packetNumber = numPoints;
+  thisDataPacket.time = Date.now().toString();
+  thisDataPacket.temp = Math.random() * yMax;  
+  dataPacketList.push(thisDataPacket);  
+  
+  
+  if (dataPacketList.length>=maxPoints+1){
+    dataPacketList.splice(0,1);
+  }
+  updateIndex();  
+  //console.log(dataPacketList[dataPacketList.length]);
+  console.log(dataPacketList);
 
   numPoints++;
-  dataPacket.index = numPoints;
-  dataPacket.time = Date.now().toString();
-  dataPacket.temp = Math.random() * yMax;
+  //clear all 
+  d3.selectAll("svg > *").remove();
 
-  dataPacketList.push(dataPacket);
   // Append SVG Object to the Page
   const svg = d3.select("#myPlot")
     .append("svg")
     .append("g")
-    .attr("transform","translate(" + 40 + "," + 40 + ")")
+    .attr("transform","translate(" + margin + "," + margin + ")")
     ;
-
   // X Axis
   const x = d3.scaleLinear()
     .domain([0, 500])
     .range([0, xMax]);
+  
+  let xAxisGenerator = d3.axisBottom(x);
+  xAxisGenerator.ticks(maxPoints);
+  xAxisGenerator.tickSize(-yMax);
+  xAxisGenerator.tickFormat(function(d,i){
+    if(dataPacketList[i]){
+      return (dataPacketList[i].index);
+    }else{
+      return "";
+    }
+  });
+
+
 
   svg.append("g")
-    .attr("transform", "translate(0," + yMax + ")")
-    .call(d3.axisBottom(x));
+    .call(xAxisGenerator)
+    .attr("transform", "translate(0," + yMax + ")")    
+  ;
 
+  svg.selectAll("text")
+  .attr("font-size","10")
+  .attr("transform","rotate(45)")
   // Y Axis
   const y = d3.scaleLinear()
     .domain([0, 500])
@@ -62,7 +105,7 @@ async function plot() {
     .selectAll("dot")
     .data(dataPacketList).enter()
     .append("circle")
-    .attr("cx", function (d) { return d.index } )
+    .attr("cx", function (d) { return (d.index*(xMax/maxPoints)) } )
     .attr("cy", function (d) { return d.temp } )
     .attr("r", 3)
     .style("fill", "Red");
@@ -74,13 +117,33 @@ async function plot() {
       .attr("stroke", "steelblue")
       .attr("stroke-width", 1.5)
       .attr("d", d3.line()
-        .x(function(d) { return (d.index) })
+        .x(function(d) { return (d.index*(xMax/maxPoints)) })
         .y(function(d) { return (d.temp) })
-        )
+        );
+    
+    //text on the points
+    svg.selectAll(".text")
+      .data(dataPacketList)                                 
+      .enter().append("text")
+      .attr("class", "text_on_point")
+      .attr("x", function(d) { return (d.index*(xMax/maxPoints)); })
+      .attr("y", function(d) { return (d.temp); })
+      .text(function(d) { return d.packetNumber; });
+
+    svg.selectAll(".text")
+      .data(dataPacketList)                                 
+      .enter().append("text")
+      .attr("class", "text_on_x_axis")
+      //.attr('transform', 'translate(' + function(d) { return (d.index*(xMax/maxPoints)); } + ', ' + 0 + ')')
+      .attr('text-anchor', 'middle')
+      .attr('transform', 'rotate(10)')
+      ;
+    
+
 }
 
 document.getElementById("update-button").addEventListener("click", function (event) {
-  event.preventDefault();
-  console.log("hello");
+  event.preventDefault(); 
+  console.log("new data");
   plot();
 });
